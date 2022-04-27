5cf65be6f7edb4e0385fc26f782807c512403963
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-04-27 22:07:19 +0200

    :package:

--- test.sh
+++ test.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+[ "$1" -le 1 ] && exit
+[ -z $2 ] && exit
+[ -f result ] && rm -f result
+
 MAX=$1
 NB_TEST=$2
 
-while [ $NB_TEST != 0 ]; do
+while [ "$NB_TEST" != 0 ]; do
 	ARG=$(seq 1 ${MAX} | shuf)
-
-	[ -f ./result ] && ./push_swap ${ARG} > result || ./push_swap ${ARG} >> result
-
+	./push_swap ${ARG} | sed -nE 's|Cycle : ([0-9]*)|\1|p' >> result
 	let "NB_TEST=${NB_TEST}-1"
 done
+
+#cat ./result | sort -u -o result
+
+result_min=$(awk 'NR==1{print $1}' result)
+result_max=$(awk 'END{print}' result)
+
+printf "for ${1} test on a list of length ${2} you have:\n"
+printf "min cycle: ${result_min}\n"
+printf "max cycle: ${result_max}\n"

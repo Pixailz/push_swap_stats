18c3355d536943c38910c35539bb9b202ca07dd4
Pixailz <Pixailz1@gmail.com>
2022-05-31 20:58:29 +0200

    :package: improve speed

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MAX=$1
-NB_TEST=$2
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#==#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+#> utils/check function
 
 # BASH COLOR :)
 blue="\x1b[38;5;75m"
@@ -11,31 +11,123 @@ green="\x1b[38;5;82m"
 blinking="\x1b[5m"
 reset="\x1b[0m"
 
-function check_checker() {
-	OLD_NB_TEST=${NB_TEST}
-	[ -f "./result_checker" ] && rm ./result_checker
-	while [ "$NB_TEST" != 0 ]; do
-		CURRENT_LIST=$(seq 1 ${MAX} | shuf | tr '\n' ' ')
-		./push_swap ${CURRENT_LIST} | ./checker_linux ${CURRENT_LIST} >> result_checker 2>>result_checker
-		let "NB_TEST=${NB_TEST}-1"
+function usage()
+{
+	[ ! -z "${1}" ] && p_warn "$1"
+	printf "Usage : ${0} -l NUMBER -n NUMBER \n"
+	printf "\t-h : show this help\n"
+	printf "\t-l : specify the length of the list\n"
+	printf "\t-n : specify the number of test to do\n"
+	exit
+}
+
+function p_warn()
+{
+	printf "[${orange}WARN ${reset}] $*\n"
+}
+
+function p_error()
+{
+	printf "[${red}ERROR${reset}] $*\n"
+	exit
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-l)
+			shift
+			if [ -z ${1} ]; then
+				usage "-l must be followed by a length"
+			else
+				LENGTH="${1}"
+			fi
+			;;
+		-n)
+			shift
+			if [ -z ${1} ]; then
+				usage "-n must be followed by the number of test"
+			else
+				NB_TEST=${1}
+			fi
+			;;
+		-h)
+			usage
+			;;
+		*)
+			usage "Wrong args"
+			;;
+	esac
+	shift
+done
+
+function check_arg()
+{
+	if [ -z ${NB_TEST} ]; then
+		usage "(-n) must be specified"
+	elif [ -z ${LENGTH} ]; then
+		usage "(-l) must be specified"
+	fi
+	case ${NB_TEST} in
+		''|*[!0-9]*)
+			usage "-n take a number in argument."
+			;;
+	esac
+	case ${LENGTH} in
+		''|*[!0-9]*)
+			usage "-l take a number in argument."
+			;;
+	esac
+}
+
+check_arg
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#==#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#==#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+#> main function
+
+function init_random_list()
+{
+	index=0
+	while [ "${index}" -lt "${NB_TEST}" ]; do
+		random_list[${index}]=$(seq 1 ${LENGTH} | shuf | tr '\n' ' ')
+		let "index=${index}+1"
 	done
+}
+
+function exec_push_swap()
+{
+	index=0
+	while [ "${index}" -lt "${NB_TEST}" ]; do
+		./push_swap ${random_list[${index}]} > tmp_cycle
+		echo $(wc -l ./tmp_cycle | cut -d' ' -f1) >> result_cycle
+		cat ./tmp_cycle | ./checker_linux ${random_list[${index}]} >> result_checker 2>>result_checker
+		let "index=${index}+1"
+	done
+}
+
+function  parse_result ()
+{
 	nb_of_ko=$(grep 'KO' ./result_checker | wc -l)
+	nb_of_error=$(grep 'Error' ./result_checker | wc -l)
+	sort -nu ./result_cycle > sorted_cycle
+	result_min=$(awk 'NR==1{print $1}' sorted_cycle)
+	result_max=$(awk 'END{print}' sorted_cycle)
+	tmp_total=$(awk '{s+=$1} END {printf "%.0f", s}' result_cycle)
+	result_med=$(echo "scale=2; ${tmp_total}/${NB_TEST}" | bc)
+}
+
+function print_result() {
 	if [ ${nb_of_ko} == 0 ]; then
 		printf "number of KO: ${green}%d${reset}\n" ${nb_of_ko}
 	else
 		printf "number of KO: ${blinking}${red}%d${reset}\n" ${nb_of_ko}
 	fi
-	nb_of_error=$(grep 'Error' ./result_checker | wc -l)
 	if [ ${nb_of_error} == 0 ]; then
 		printf "number of Error: ${green}%d${reset}\n" ${nb_of_error}
 	else
 		printf "number of Error: ${blinking}${red}%d${reset}\n" ${nb_of_error}
 	fi
-	NB_TEST=${OLD_NB_TEST}
-}
-
-function print_result() {
-	printf "for ${blue}${NB_TEST}${reset} test on a list of length ${blue}${MAX}${reset} you have:\n"
+	printf "for ${blue}${NB_TEST}${reset} test on a list of length ${blue}${LENGTH}${reset} you have:\n"
 	printf "${green}min${reset} cycle: ${orange}${result_min}${reset}\n"
 	if [ ${result_med:0:1} == "." ]; then
 		result_med="0${result_med}"
@@ -44,37 +136,22 @@ function print_result() {
 	printf "${red}max${reset} cycle: ${orange}${result_max}${reset}\n"
 }
 
-function check_cycle() {
-	OLD_NB_TEST=${NB_TEST}
-	[ -f "./result_cycle" ] && rm ./result_cycle
-	[ -f "./tmp_cycle" ] && rm ./tmp_cycle
-	while [ "$NB_TEST" != 0 ]; do
-		CURRENT_LIST=$(seq 1 ${MAX} | shuf | tr '\n' ' ')
-		./push_swap ${CURRENT_LIST} > tmp_cycle
-		echo $(wc -l ./tmp_cycle | cut -d' ' -f1) >> result_cycle
-		let "NB_TEST=${NB_TEST}-1"
-	done
-	sort -nu ./result_cycle > sorted_cycle
-	result_min=$(awk 'NR==1{print $1}' sorted_cycle)
-	result_max=$(awk 'END{print}' sorted_cycle)
-	NB_TEST=${OLD_NB_TEST}
-	tmp_total=$(awk '{s+=$1} END {printf "%.0f", s}' result_cycle)
-	result_med=$(echo "scale=2; ${tmp_total}/${NB_TEST}" | bc)
-	print_result
-}
-
 function clean_file() {
-	[ -f "./result_checker" ] && rm ./result_checker
-	[ -f "./result_cycle" ] && rm ./result_cycle
-	[ -f "./tmp_cycle" ] && rm ./tmp_cycle
-	[ -f "./sorted_cycle" ] && rm ./sorted_cycle
+	[ -f "./result_checker" ] && rm -f ./result_checker
+	[ -f "./result_cycle" ] && rm -f ./result_cycle
+	[ -f "./tmp_cycle" ] && rm -f ./tmp_cycle
+	[ -f "./sorted_cycle" ] && rm -f ./sorted_cycle
 }
 
-function main()
+function main ()
 {
-	check_checker
-	check_cycle
+	clean_file
+	init_random_list
+	exec_push_swap
+	parse_result
+	print_result
 	clean_file
 }
 
 main
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#==#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#

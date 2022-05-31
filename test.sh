e7fcb7faa10d40458b8c4037cc1cc5375f71d87c
Pixailz <Pixailz1@gmail.com>
2022-05-31 19:17:37 +0200

    :package: test.sh: add color and medium cycle

--- test.sh
+++ test.sh
@@ -3,23 +3,45 @@
 MAX=$1
 NB_TEST=$2
 
+# BASH COLOR :)
+blue="\x1b[38;5;75m"
+red="\x1b[38;5;196m"
+orange="\x1b[38;5;214m"
+green="\x1b[38;5;82m"
+blinking="\x1b[5m"
+reset="\x1b[0m"
+
 function check_checker() {
 	OLD_NB_TEST=${NB_TEST}
 	[ -f "./result_checker" ] && rm ./result_checker
 	while [ "$NB_TEST" != 0 ]; do
 		CURRENT_LIST=$(seq 1 ${MAX} | shuf | tr '\n' ' ')
-		./push_swap ${CURRENT_LIST} | ./checker_linux ${CURRENT_LIST} >> result_checker
+		./push_swap ${CURRENT_LIST} | ./checker_linux ${CURRENT_LIST} >> result_checker 2>>result_checker
 		let "NB_TEST=${NB_TEST}-1"
 	done
 	nb_of_ko=$(grep 'KO' ./result_checker | wc -l)
-	printf 'number of KO: %d\n'
+	if [ ${nb_of_ko} == 0 ]; then
+		printf "number of KO: ${green}%d${reset}\n" ${nb_of_ko}
+	else
+		printf "number of KO: ${blinking}${red}%d${reset}\n" ${nb_of_ko}
+	fi
+	nb_of_error=$(grep 'Error' ./result_checker | wc -l)
+	if [ ${nb_of_error} == 0 ]; then
+		printf "number of Error: ${green}%d${reset}\n" ${nb_of_error}
+	else
+		printf "number of Error: ${blinking}${red}%d${reset}\n" ${nb_of_error}
+	fi
 	NB_TEST=${OLD_NB_TEST}
 }
 
 function print_result() {
-	printf "for ${NB_TEST} test on a list of length ${MAX} you have:\n"
-	printf "min cycle: ${result_min}\n"
-	printf "max cycle: ${result_max}\n"
+	printf "for ${blue}${NB_TEST}${reset} test on a list of length ${blue}${MAX}${reset} you have:\n"
+	printf "${green}min${reset} cycle: ${orange}${result_min}${reset}\n"
+	if [ ${result_med:0:1} == "." ]; then
+		result_med="0${result_med}"
+	fi
+	printf "${orange}med${reset} cycle: ${orange}${result_med}${reset}\n"
+	printf "${red}max${reset} cycle: ${orange}${result_max}${reset}\n"
 }
 
 function check_cycle() {
@@ -32,10 +54,12 @@ function check_cycle() {
 		echo $(wc -l ./tmp_cycle | cut -d' ' -f1) >> result_cycle
 		let "NB_TEST=${NB_TEST}-1"
 	done
-	sort -u ./result_cycle > sorted_cycle
+	sort -nu ./result_cycle > sorted_cycle
 	result_min=$(awk 'NR==1{print $1}' sorted_cycle)
 	result_max=$(awk 'END{print}' sorted_cycle)
 	NB_TEST=${OLD_NB_TEST}
+	tmp_total=$(awk '{s+=$1} END {printf "%.0f", s}' result_cycle)
+	result_med=$(echo "scale=2; ${tmp_total}/${NB_TEST}" | bc)
 	print_result
 }
 
